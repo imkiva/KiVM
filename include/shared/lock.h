67ab34ab03096f800fca79c7da533153ed938741
Michael Lee <mikecovlee@163.com>
2018-02-25 14:44:46 +0800

    add namespace

--- include/shared/lock.h
+++ include/shared/lock.h
@@ -1,13 +1,13 @@
 //
 // Created by kiva on 2018/2/25.
+// Modified by mikecovlee on 2018/2/25.
 //
 
-#ifndef KIVAVM_LOCK_H
-#define KIVAVM_LOCK_H
+#pragma once
 
 #include <mutex>
 
-using Lock = std::mutex;
-using LockGuard = std::lock_guard<std::mutex>;
-
-#endif //KIVAVM_LOCK_H
+namespace kivm {
+	using Lock = std::mutex;
+	using LockGuard = std::lock_guard<std::mutex>;
+}
--- include/shared/monitor.h
+++ include/shared/monitor.h
@@ -1,65 +1,65 @@
 //
 // Created by kiva on 2018/2/25.
+// Modified by mikecovlee on 2018/2/25.
 //
 
-#ifndef KIVAVM_MONITOR_H
-#define KIVAVM_MONITOR_H
+#pragma once
 
 #include <condition_variable>
 #include <thread>
 #include <mutex>
 #include <chrono>
 
-class Monitor final {
-private:
-	std::mutex _mutex;
-	std::condition_variable _cond;
-	std::unique_lock<std::mutex> _lock;
+namespace kivm {
+	class Monitor final {
+	private:
+		std::mutex _mutex;
+		std::condition_variable _cond;
+		std::unique_lock<std::mutex> _lock;
 
-public:
-	Monitor() : _lock(_mutex) {}
+	public:
+		Monitor() : _lock(_mutex) {}
 
-	Monitor(const Monitor &) = delete;
+		Monitor(const Monitor &) = delete;
 
-	Monitor(Monitor &&) noexcept = delete;
+		Monitor(Monitor &&) noexcept = delete;
 
-	~Monitor() = default;
+		~Monitor() = default;
 
-	void enter()
-	{
-		_mutex.lock();
-	}
+		void enter()
+		{
+			_mutex.lock();
+		}
 
-	void wait()
-	{
-		_cond.wait(_lock);
-	}
+		void wait()
+		{
+			_cond.wait(_lock);
+		}
 
-	void wait(std::size_t time)
-	{
-		_cond.wait_for(_lock, std::chrono::milliseconds(time));
-	}
+		void wait(std::size_t time)
+		{
+			_cond.wait_for(_lock, std::chrono::milliseconds(time));
+		}
 
-	void notify()
-	{
-		_cond.notify_one();
-	}
+		void notify()
+		{
+			_cond.notify_one();
+		}
 
-	void notify_all()
-	{
-		_cond.notify_all();
-	}
+		void notify_all()
+		{
+			_cond.notify_all();
+		}
 
-	void leave()
-	{
-		_mutex.unlock();
-	}
+		void leave()
+		{
+			_mutex.unlock();
+		}
 
-	void force_unlock_when_athrow()
-	{
-		_mutex.try_lock();
-		_mutex.unlock();
-	}
-};
-
-#endif //KIVAVM_MONITOR_H
+		void force_unlock_when_athrow()
+		{
+			_mutex.try_lock();
+			_mutex.unlock();
+		}
+	};
+}
