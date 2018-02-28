01b5ce5732669c55a894247a39ba0a569ec0f22e
imkiva <libkernelpanic@gmail.com>
2018-03-01 03:45:47 +0800

    attributeInfo: support parsing all attributes

--- include/kivm/classfile/attributeInfo.h
+++ include/kivm/classfile/attributeInfo.h
@@ -374,6 +374,176 @@ namespace kivm {
         ~MethodParameters_attribute() override;
     };
 
+
+    struct value_t {
+        virtual ~value_t() = default;
+    };
+
+    struct const_value_t : public value_t {
+        u2 const_value_index;
+    };
+
+    struct enum_const_value_t : public value_t {
+        u2 type_name_index;
+        u2 const_name_index;
+    };
+
+    struct class_info_t : public value_t {
+        u2 class_info_index;
+    };
+
+    struct element_value {
+        u1 tag;
+        value_t *value = nullptr;
+
+        ~element_value();
+    };
+
+    struct element_value_pairs_t {
+        u2 element_name_index;
+        element_value value;
+    };
+
+    struct annotation : public value_t {
+        u2 type_index;
+        u2 num_element_value_pairs;
+        element_value_pairs_t *element_value_pairs = nullptr;
+
+        ~annotation() override;
+    };
+
+    struct array_value_t : public value_t {
+        u2 num_values;
+        element_value *values = nullptr;
+
+        ~array_value_t() override;
+    };
+
+    struct type_annotation {
+        // target_type
+        struct target_info_t {
+            virtual ~target_info_t() = default;
+        };
+
+        struct type_parameter_target : target_info_t {
+            u1 type_parameter_index;
+        };
+
+        struct supertype_target : target_info_t {
+            u2 supertype_index;
+        };
+
+        struct type_parameter_bound_target : target_info_t {
+            u1 type_parameter_index;
+            u1 bound_index;
+        };
+
+        struct empty_target : target_info_t {
+        };
+
+        struct formal_parameter_target : target_info_t {
+            u1 formal_parameter_index;
+        };
+
+        struct throws_target : target_info_t {
+            u2 throws_type_index;
+        };
+
+        struct table_t {
+            u2 start_pc;
+            u2 length;
+            u2 index;
+        };
+
+        struct localvar_target : target_info_t {
+            u2 table_length;
+            table_t *table = nullptr;
+
+            ~localvar_target() override;
+        };
+
+        struct catch_target : target_info_t {
+            u2 exception_table_index;
+        };
+
+        struct offset_target : target_info_t {
+            u2 offset;
+        };
+
+        struct type_argument_target : target_info_t {
+            u2 offset;
+            u1 type_argument_index;
+        };
+
+        struct path_t {
+            u1 type_path_kind;
+            u1 type_argument_index;
+        };
+
+        // type_path
+        struct type_path {
+            u1 path_length;
+            path_t *path = nullptr;
+
+            ~type_path();
+        };
+
+        // basic
+        u1 target_type;
+        target_info_t *target_info = nullptr;
+        type_path target_path;
+        annotation *annotations = nullptr;
+
+        ~type_annotation();
+    };
+
+    struct parameter_annotations_t {
+        u2 num_annotations;
+        annotation *annotations = nullptr;
+
+        ~parameter_annotations_t();
+    };
+
+    struct RuntimeVisibleAnnotations_attribute : public attribute_info {
+        parameter_annotations_t parameter_annotations;
+    };
+
+    struct RuntimeInvisibleAnnotations_attribute : public attribute_info {
+        parameter_annotations_t parameter_annotations;
+    };
+
+    struct RuntimeVisibleParameterAnnotations_attribute : public attribute_info {
+        u1 num_parameters;
+        parameter_annotations_t *parameter_annotations = nullptr;
+
+        ~RuntimeVisibleParameterAnnotations_attribute() override;
+    };
+
+    struct RuntimeInvisibleParameterAnnotations_attribute : public attribute_info {
+        u1 num_parameters;
+        parameter_annotations_t *parameter_annotations = nullptr;
+
+        ~RuntimeInvisibleParameterAnnotations_attribute() override;
+    };
+
+    struct RuntimeVisibleTypeAnnotations_attribute : public attribute_info {
+        u2 num_annotations;
+        type_annotation *annotations = nullptr;
+
+        ~RuntimeVisibleTypeAnnotations_attribute() override;
+    };
+
+    struct RuntimeInvisibleTypeAnnotations_attribute : public attribute_info {
+        u2 num_annotations;
+        type_annotation *annotations = nullptr;
+
+        ~RuntimeInvisibleTypeAnnotations_attribute() override;
+    };
+
+    struct AnnotationDefault_attribute : public attribute_info {
+        element_value default_value;
+    };
+
     class AttributeParser {
     public:
         static void read_attributes(attribute_info ***p, u2 count,
--- src/kivm/classfile/attributeInfo.cpp
+++ src/kivm/classfile/attributeInfo.cpp
@@ -126,22 +126,13 @@ namespace kivm {
     ClassFileStream &operator>>(ClassFileStream &stream, StackMapTable_attribute::append_frame &info) {
         info.frame_type = stream.get_u1();
         info.offset_delta = stream.get_u2();
-        if (info.frame_type - 251 != 0) {
+        if (info.frame_type - 251 >= 0) {
             info.locals = new StackMapTable_attribute::verification_type_info *[info.frame_type - 251];
         }
         for (int i = 0; i < info.frame_type - 251; i++) {
             info.locals[i] = parse_verification_type(stream);
         }
         return stream;
-    }
-
-    StackMapTable_attribute::append_frame::~append_frame() {
-        if (locals != nullptr) {
-            for (int i = 0; i < frame_type - 251; i++) {
-                delete locals[i];
-            }
-            delete[] locals;
-        }
     }
 
     ClassFileStream &operator>>(ClassFileStream &stream, StackMapTable_attribute::full_frame &info) {
@@ -331,6 +322,293 @@ namespace kivm {
         }
         return stream;
     }
+
+    ClassFileStream &operator>>(ClassFileStream &, element_value_pairs_t &);
+
+    ClassFileStream &operator>>(ClassFileStream &, array_value_t &);
+
+    ClassFileStream &operator>>(ClassFileStream &stream, const_value_t &info) {
+        info.const_value_index = stream.get_u2();
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, enum_const_value_t &info) {
+        info.type_name_index = stream.get_u2();
+        info.const_name_index = stream.get_u2();
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, class_info_t &info) {
+        info.class_info_index = stream.get_u2();
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, annotation &info) {
+        info.type_index = stream.get_u2();
+        info.num_element_value_pairs = stream.get_u2();
+        info.element_value_pairs = new element_value_pairs_t[info.num_element_value_pairs];
+        for (int i = 0; i < info.num_element_value_pairs; i++) {
+            stream >> info.element_value_pairs[i];
+        }
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, element_value &info) {
+        info.tag = stream.get_u1();
+        switch ((char) info.tag) {
+            case 'B':
+            case 'C':
+            case 'D':
+            case 'F':
+            case 'I':
+            case 'J':
+            case 'S':
+            case 'Z':
+            case 's': {
+                info.value = new const_value_t;
+                stream >> *(const_value_t *) info.value;
+                break;
+            }
+            case 'e': {
+                info.value = new enum_const_value_t;
+                stream >> *(enum_const_value_t *) info.value;
+                break;
+            }
+            case 'c': {
+                info.value = new class_info_t;
+                stream >> *(class_info_t *) info.value;
+                break;
+            }
+            case '@': {
+                info.value = new annotation;
+                stream >> *(annotation *) info.value;
+                break;
+            }
+            case '[': {
+                info.value = new array_value_t;
+                stream >> *(array_value_t *) info.value;
+                break;
+            }
+            default: {
+                assert(false);
+            }
+        }
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, element_value_pairs_t &info) {
+        info.element_name_index = stream.get_u2();
+        stream >> info.value;
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, array_value_t &info) {
+        info.num_values = stream.get_u2();
+        info.values = new element_value[info.num_values];
+        for (int i = 0; i < info.num_values; i++) {
+            stream >> info.values[i];
+        }
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, type_annotation::type_parameter_target &info) {
+        info.type_parameter_index = stream.get_u1();
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, type_annotation::supertype_target &info) {
+        info.supertype_index = stream.get_u2();
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, type_annotation::type_parameter_bound_target &info) {
+        info.type_parameter_index = stream.get_u1();
+        info.bound_index = stream.get_u1();
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, type_annotation::empty_target &info) {
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, type_annotation::formal_parameter_target &info) {
+        info.formal_parameter_index = stream.get_u1();
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, type_annotation::throws_target &info) {
+        info.throws_type_index = stream.get_u2();
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, type_annotation::table_t &info) {
+        info.start_pc = stream.get_u2();
+        info.length = stream.get_u2();
+        info.index = stream.get_u2();
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, type_annotation::localvar_target &info) {
+        info.table_length = stream.get_u2();
+        info.table = new type_annotation::table_t[info.table_length];
+        for (int i = 0; i < info.table_length; i++) {
+            stream >> info.table[i];
+        }
+
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, type_annotation::catch_target &info) {
+        info.exception_table_index = stream.get_u2();
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, type_annotation::offset_target &info) {
+        info.offset = stream.get_u2();
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, type_annotation::type_argument_target &info) {
+        info.offset = stream.get_u2();
+        info.type_argument_index = stream.get_u1();
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, type_annotation::path_t &info) {
+        info.type_path_kind = stream.get_u1();
+        info.type_argument_index = stream.get_u1();
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, type_annotation::type_path &info) {
+        info.path_length = stream.get_u1();
+        info.path = new type_annotation::path_t[info.path_length];
+        for (int i = 0; i < info.path_length; i++) {
+            stream >> info.path[i];
+        }
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, type_annotation &info) {
+        info.target_type = stream.get_u1();
+        if (info.target_type == 0x00 || info.target_type == 0x01) {
+            auto *result = new type_annotation::type_parameter_target;
+            stream >> *result;
+            info.target_info = result;
+        } else if (info.target_type == 0x10) {
+            auto *result = new type_annotation::supertype_target;
+            stream >> *result;
+            info.target_info = result;
+        } else if (info.target_type == 0x11 || info.target_type == 0x12) {
+            auto *result = new type_annotation::type_parameter_bound_target;
+            stream >> *result;
+            info.target_info = result;
+        } else if (info.target_type == 0x13 || info.target_type == 0x14 || info.target_type == 0x15) {
+            auto *result = new type_annotation::empty_target;
+            stream >> *result;
+            info.target_info = result;
+        } else if (info.target_type == 0x16) {
+            auto *result = new type_annotation::formal_parameter_target;
+            stream >> *result;
+            info.target_info = result;
+        } else if (info.target_type == 0x17) {
+            auto *result = new type_annotation::throws_target;
+            stream >> *result;
+            info.target_info = result;
+        } else if (info.target_type == 0x40 || info.target_type == 0x41) {
+            auto *result = new type_annotation::localvar_target;
+            stream >> *result;
+            info.target_info = result;
+        } else if (info.target_type == 0x42) {
+            auto *result = new type_annotation::catch_target;
+            stream >> *result;
+            info.target_info = result;
+        } else if (info.target_type == 0x43 || info.target_type == 0x44 || info.target_type == 0x45 ||
+                   info.target_type == 0x46) {
+            auto *result = new type_annotation::offset_target;
+            stream >> *result;
+            info.target_info = result;
+        } else if (info.target_type == 0x47 || info.target_type == 0x48 || info.target_type == 0x49 ||
+                   info.target_type == 0x4A || info.target_type == 0x4B) {
+            auto *result = new type_annotation::type_argument_target;
+            stream >> *result;
+            info.target_info = result;
+        } else {
+            assert(false);
+        }
+        stream >> info.target_path;
+        info.annotations = new annotation;
+        stream >> *info.annotations;
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, parameter_annotations_t &info) {
+        info.num_annotations = stream.get_u2();
+        info.annotations = new annotation[info.num_annotations];
+        for (int i = 0; i < info.num_annotations; i++) {
+            stream >> info.annotations[i];
+        }
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, RuntimeVisibleAnnotations_attribute &info) {
+        stream >> *((attribute_info *) &info);
+        stream >> info.parameter_annotations;
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, RuntimeInvisibleAnnotations_attribute &info) {
+        stream >> *((attribute_info *) &info);
+        stream >> info.parameter_annotations;
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, RuntimeVisibleParameterAnnotations_attribute &info) {
+        stream >> *((attribute_info *) &info);
+        info.num_parameters = stream.get_u1();
+        info.parameter_annotations = new parameter_annotations_t[info.num_parameters];
+        for (int i = 0; i < info.num_parameters; i++) {
+            stream >> info.parameter_annotations[i];
+        }
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, RuntimeInvisibleParameterAnnotations_attribute &info) {
+        stream >> *((attribute_info *) &info);
+        info.num_parameters = stream.get_u1();
+        info.parameter_annotations = new parameter_annotations_t[info.num_parameters];
+        for (int i = 0; i < info.num_parameters; i++) {
+            stream >> info.parameter_annotations[i];
+        }
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, RuntimeVisibleTypeAnnotations_attribute &info) {
+        stream >> *((attribute_info *) &info);
+        info.num_annotations = stream.get_u2();
+        info.annotations = new type_annotation[info.num_annotations];
+        for (int i = 0; i < info.num_annotations; i++) {
+            stream >> info.annotations[i];
+        }
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, RuntimeInvisibleTypeAnnotations_attribute &info) {
+        stream >> *((attribute_info *) &info);
+        info.num_annotations = stream.get_u2();
+        info.annotations = new type_annotation[info.num_annotations];
+        for (int i = 0; i < info.num_annotations; i++) {
+            stream >> info.annotations[i];
+        }
+        return stream;
+    }
+
+    ClassFileStream &operator>>(ClassFileStream &stream, AnnotationDefault_attribute &info) {
+        stream >> *((attribute_info *) &info);
+        stream >> info.default_value;
+        return stream;
+    }
+
 
     /*******************************************************************
      * helpers
@@ -588,6 +866,60 @@ namespace kivm {
         delete[] parameters;
     }
 
+    array_value_t::~array_value_t() {
+        delete[] values;
+    }
+
+    element_value::~element_value() {
+        delete value;
+    }
+
+    annotation::~annotation() {
+        delete[] element_value_pairs;
+    }
+
+    type_annotation::localvar_target::~localvar_target() {
+        delete[] table;
+    }
+
+    type_annotation::type_path::~type_path() {
+        delete[] path;
+    }
+
+    type_annotation::~type_annotation() {
+        delete target_info;
+        delete annotations;
+    }
+
+    parameter_annotations_t::~parameter_annotations_t() {
+        delete[] annotations;
+    }
+
+    RuntimeVisibleParameterAnnotations_attribute::~RuntimeVisibleParameterAnnotations_attribute() {
+        delete[] parameter_annotations;
+    }
+
+    RuntimeInvisibleParameterAnnotations_attribute::~RuntimeInvisibleParameterAnnotations_attribute() {
+        delete[] parameter_annotations;
+    }
+
+    RuntimeVisibleTypeAnnotations_attribute::~RuntimeVisibleTypeAnnotations_attribute() {
+        delete[] annotations;
+    }
+
+    RuntimeInvisibleTypeAnnotations_attribute::~RuntimeInvisibleTypeAnnotations_attribute() {
+        delete[] annotations;
+    }
+
+    StackMapTable_attribute::append_frame::~append_frame() {
+        if (locals != nullptr) {
+            for (int i = 0; i < frame_type - 251; i++) {
+                delete locals[i];
+            }
+            delete[] locals;
+        }
+    }
+
     /*******************************************************************
      * Attribute parser
      *******************************************************************/
@@ -694,6 +1026,27 @@ namespace kivm {
             case ATTRIBUTE_MethodParameters:
                 return read_attribute_entry<MethodParameters_attribute>(stream);
 
+            case ATTRIBUTE_RuntimeVisibleAnnotations:
+                return read_attribute_entry<RuntimeVisibleAnnotations_attribute>(stream);
+
+            case ATTRIBUTE_RuntimeInvisibleAnnotations:
+                return read_attribute_entry<RuntimeInvisibleAnnotations_attribute>(stream);
+
+            case ATTRIBUTE_RuntimeVisibleParameterAnnotations:
+                return read_attribute_entry<RuntimeVisibleParameterAnnotations_attribute>(stream);
+
+            case ATTRIBUTE_RuntimeInvisibleParameterAnnotations:
+                return read_attribute_entry<RuntimeInvisibleParameterAnnotations_attribute>(stream);
+
+            case ATTRIBUTE_RuntimeVisibleTypeAnnotations:
+                return read_attribute_entry<RuntimeVisibleTypeAnnotations_attribute>(stream);
+
+            case ATTRIBUTE_RuntimeInvisibleTypeAnnotations:
+                return read_attribute_entry<RuntimeInvisibleTypeAnnotations_attribute>(stream);
+
+            case ATTRIBUTE_AnnotationDefault:
+                return read_attribute_entry<AnnotationDefault_attribute>(stream);
+            
             case ATTRIBUTE_INVALID:
                 fprintf(stderr, "Invalid attribute tag: %u\n", attribute_tag);
                 exit(1);
@@ -703,27 +1056,6 @@ namespace kivm {
                 fprintf(stderr, "Unimplemented attribute: %u\n", attribute_tag);
                 exit(1);
                 return nullptr;
-
-//            case ATTRIBUTE_RuntimeVisibleAnnotations:
-//                return read_attribute_entry<RuntimeVisibleAnnotations_attribute>(stream);
-//
-//            case ATTRIBUTE_RuntimeInvisibleAnnotations:
-//                return read_attribute_entry<RuntimeInvisibleAnnotations_attribute>(stream);
-//
-//            case ATTRIBUTE_RuntimeVisibleParameterAnnotations:
-//                return read_attribute_entry<RuntimeVisibleParameterAnnotations_attribute>(stream);
-//
-//            case ATTRIBUTE_RuntimeInvisibleParameterAnnotations:
-//                return read_attribute_entry<RuntimeInvisibleParameterAnnotations_attribute>(stream);
-//
-//            case ATTRIBUTE_RuntimeVisibleTypeAnnotations:
-//                return read_attribute_entry<RuntimeVisibleTypeAnnotations_attribute>(stream);
-//
-//            case ATTRIBUTE_RuntimeInvisibleTypeAnnotations:
-//                return read_attribute_entry<RuntimeInvisibleTypeAnnotations_attribute>(stream);
-//
-//            case ATTRIBUTE_AnnotationDefault:
-//                return read_attribute_entry<AnnotationDefault_attribute>(stream);
         }
         return nullptr;
     }
