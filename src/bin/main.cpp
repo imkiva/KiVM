67798986e2b4e98f9d3b0e331a53d5a5e265eb53
imkiva <libkernelpanic@gmail.com>
2018-03-01 03:58:40 +0800

    Fix: arrayKlass down dimension type error

--- src/bin/main.cpp
+++ src/bin/main.cpp
@@ -5,5 +5,18 @@ int main() {
     using namespace kivm;
     auto *a = (InstanceKlass *) BootstrapClassLoader::get()
             ->loadClass(L"java/lang/String");
+
+    auto *b = (InstanceKlass *) BootstrapClassLoader::get()
+            ->loadClass(L"[Ljava/lang/String;");
+
+    auto *c = (InstanceKlass *) BootstrapClassLoader::get()
+            ->loadClass(L"[[[[[Ljava/lang/String;");
+
+    auto *d = (InstanceKlass *) BootstrapClassLoader::get()
+            ->loadClass(L"[J");
+
+    auto *e = (InstanceKlass *) BootstrapClassLoader::get()
+            ->loadClass(L"[[[[[J");
+
     return 0;
 }
--- src/kivm/oop/arrayKlass.cpp
+++ src/kivm/oop/arrayKlass.cpp
@@ -20,20 +20,24 @@ namespace kivm {
     TypeArrayKlass::TypeArrayKlass(ClassLoader *class_loader, int dimension, ValueType component_type)
             : ArrayKlass(class_loader, dimension, ClassType::TYPE_ARRAY_CLASS) {
         this->_component_type = component_type;
+        this->_down_dimension_type = nullptr;
     }
 
     TypeArrayKlass::TypeArrayKlass(ClassLoader *class_loader, TypeArrayKlass *down_type)
             : TypeArrayKlass(class_loader, down_type->get_dimension() + 1,
                              down_type->get_component_type()) {
+        this->_down_dimension_type = down_type;
     }
 
     ObjectArrayKlass::ObjectArrayKlass(ClassLoader *class_loader, int dimension, InstanceKlass *component_type)
             : ArrayKlass(class_loader, dimension, ClassType::OBJECT_ARRAY_CLASS) {
         this->_component_type = component_type;
+        this->_down_dimension_type = nullptr;
     }
 
     ObjectArrayKlass::ObjectArrayKlass(ClassLoader *class_loader, ObjectArrayKlass *down_type)
             : ObjectArrayKlass(class_loader, down_type->get_dimension() + 1,
                                down_type->get_component_type()) {
+        this->_down_dimension_type = down_type;
     }
 }
