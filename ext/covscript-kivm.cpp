22f9871c615f20d20190b0ee9314b85a4dbcef4c
imkiva <libkernelpanic@gmail.com>
2018-03-01 23:26:11 +0800

    Ext: new extension helper

--- ext/covscript-kivm.cpp
+++ ext/covscript-kivm.cpp
@@ -2,52 +2,9 @@
 // Created by kiva on 2018/3/1.
 //
 
-#include <covscript/cni.hpp>
-#include <covscript/extension.hpp>
-
-#define __CNI_NAME_MIXER(PREFIX, NAME) static cni_register PREFIX##NAME
-#define __CNI_REGISTER(NAME, ARGS) __CNI_NAME_MIXER(_cni_register_, NAME)(#NAME, NAME, ARGS);
-#define __CNI_NORMAL(name) __CNI_REGISTER(name, false)
-#define __CNI_CONST(name) __CNI_REGISTER(name, true)
-
-#define __CNI_EXPAND(MARCO, R, NAME, ...) \
-    extern R NAME(__VA_ARGS__); \
-    MARCO(NAME); \
-    R NAME(__VA_ARGS__)
-
-#define __CS_EXTENSION_NAME(NAME) __cs_extension_ext_for_##NAME
-#define __CS_SHARED_EXTENSION_NAME(NAME) __cs_extension_shared_ext_for_##NAME
-
-#define CS_EXTENSION(NAME) \
-    static cs::extension __CS_EXTENSION_NAME(NAME); \
-    static cs::extension_t __CS_SHARED_EXTENSION_NAME(NAME) = cs::make_shared_extension(__CS_EXTENSION_NAME(NAME)); \
-    class cni_register final { \
-    public: \
-    template<typename _fT> \
-    cni_register(const char *name, _fT &&func, bool is_const) { \
-        using namespace cs; \
-        __CS_EXTENSION_NAME(NAME).add_var(name, var::make_protect<callable>(cni(func), is_const)); \
-    } \
-    }; \
-    cs::extension *cs_extension() { \
-        return &__CS_EXTENSION_NAME(NAME); \
-    }
-
-#define CS_ENABLE_THIS(NAME, TYPE) \
-    namespace cs_impl { \
-        template<> \
-        cs::extension_t &get_ext<TYPE>() { \
-            return __CS_SHARED_EXTENSION_NAME(NAME); \
-        } \
-    }
-
-#define CNI_NORMAL(R, NAME, ...) __CNI_EXPAND(__CNI_NORMAL, R, NAME, ##__VA_ARGS__)
-#define CNI_CONST(R, NAME, ...) __CNI_EXPAND(__CNI_CONST, R, NAME, ##__VA_ARGS__)
-
+#include "extension_helper.h"
 
 CS_EXTENSION(kivm)
-
-namespace kivm_cs {
     struct kivm_holder {
     };
 
@@ -62,7 +19,7 @@ namespace kivm_cs {
     CNI_NORMAL(void, greet2, cs::number x, cs::number y) {
         printf("Hello World: x = %d, y = %d\n", static_cast<int>(x), static_cast<int>(y));
     }
-}
 
-CS_ENABLE_THIS(kivm, kivm_cs::kivm_holder)
+    CS_ENABLE_THIS(kivm, kivm_holder)
 
+CS_EXTENSION_END()
---
+++ ext/extension_helper.h
@@ -0,0 +1,52 @@
+//
+// Created by kiva on 2018/3/1.
+//
+
+#pragma once
+#include <covscript/cni.hpp>
+#include <covscript/extension.hpp>
+
+#define __CS_EXTENSION_NS_NAME(NAME) cs_ext_##NAME
+#define __CNI_NAME_MIXER(PREFIX, NAME) static cni_register PREFIX##NAME
+#define __CNI_REGISTER(NAME, ARGS) __CNI_NAME_MIXER(_cni_register_, NAME)(#NAME, NAME, ARGS);
+#define __CNI_NORMAL(NAME) __CNI_REGISTER(NAME, false)
+#define __CNI_CONST(NAME) __CNI_REGISTER(NAME, true)
+
+#define __CNI_EXPAND(MARCO, R, NAME, ...) \
+    extern R NAME(__VA_ARGS__); \
+    MARCO(NAME); \
+    R NAME(__VA_ARGS__)
+
+#define CS_EXTENSION(NAME) \
+    namespace __CS_EXTENSION_NS_NAME(NAME) { \
+    static cs::extension __cs_extension_ext; \
+    static cs::extension_t __cs_extension_shared = cs::make_shared_extension(__cs_extension_ext); \
+    class cni_register final { \
+    public: \
+        template<typename _fT> \
+        cni_register(const char *name, _fT &&func, bool is_const) { \
+            using namespace cs; \
+            __cs_extension_ext.add_var(name, cs::var::make_protect<cs::callable>(cs::cni(func), is_const)); \
+        } \
+    }; \
+    } \
+    cs::extension *cs_extension() { \
+        return &__CS_EXTENSION_NS_NAME(NAME)::__cs_extension_ext; \
+    } \
+    namespace __CS_EXTENSION_NS_NAME(NAME) {
+
+#define CS_EXTENSION_END() }
+
+#define CS_ENABLE_THIS(NAME, TYPE) \
+    } \
+    namespace cs_impl { \
+        template<> \
+        cs::extension_t &get_ext<__CS_EXTENSION_NS_NAME(NAME)::TYPE>() { \
+            return __CS_EXTENSION_NS_NAME(NAME)::__cs_extension_shared; \
+        } \
+    } \
+    namespace __CS_EXTENSION_NS_NAME(NAME) {
+
+#define CNI_NORMAL(R, NAME, ...) __CNI_EXPAND(__CNI_NORMAL, R, NAME, ##__VA_ARGS__)
+#define CNI_CONST(R, NAME, ...) __CNI_EXPAND(__CNI_CONST, R, NAME, ##__VA_ARGS__)
+
