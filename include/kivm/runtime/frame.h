128969f64b0643b4267646cd37d130971fd6de64
imkiva <libkernelpanic@gmail.com>
2018-03-23 15:14:01 +0800

    runtime: frame list

--- include/kivm/runtime/frame.h
+++ include/kivm/runtime/frame.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include <kivm/runtime/stack.h>
+#include <cassert>
 
 namespace kivm {
     struct Frame {
@@ -12,5 +13,48 @@ namespace kivm {
         Stack _stack;
 
         Frame(int max_locals, int max_stacks);
+    };
+
+    struct FrameList {
+        int _max_frames;
+        int _size;
+        Frame *_current;
+
+        explicit FrameList(int max_frames);
+
+        inline void push(Frame *frame) {
+            if (_size >= _max_frames) {
+                // TODO: throw java.lang.StackOverflowException
+                assert(false);
+            }
+
+            frame->_previous = _current;
+            _current = frame;
+            ++_size;
+        }
+
+        inline Frame *pop() {
+            if (_size == 0 || _current == nullptr) {
+                // TODO: throw java.lang.StackOverflowException
+                assert(false);
+            }
+
+            Frame *current = _current;
+            _current = _current->_previous;
+            --_size;
+
+            // this frame is not longer belong to this list.
+            current->_previous = nullptr;
+            return current;
+        }
+
+        inline Frame *current() const {
+            if (_size == 0 || _current == nullptr) {
+                // TODO: panic()
+                assert(false);
+            }
+
+            return _current;
+        }
     };
 }
--- src/kivm/runtime/frame.cpp
+++ src/kivm/runtime/frame.cpp
@@ -7,4 +7,8 @@ namespace kivm {
     Frame::Frame(int max_locals, int max_stacks)
             : _locals(max_locals), _stack(max_stacks) {
     }
+
+    FrameList::FrameList(int max_frames)
+            : _max_frames(max_frames), _size(0), _current(nullptr) {
+    }
 }
