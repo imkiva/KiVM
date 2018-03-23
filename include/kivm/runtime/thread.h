cc9b54e607274bd631ff2ceaea2c79eb9f0e8cdb
imkiva <libkernelpanic@gmail.com>
2018-03-23 15:19:24 +0800

    runtime: thread mirrors java thread

--- include/kivm/runtime/thread.h
+++ include/kivm/runtime/thread.h
@@ -3,12 +3,26 @@
 //
 #pragma once
 
+#include <kivm/oop/instanceOop.h>
 #include <kivm/runtime/stack.h>
+#include <unordered_map>
+#include <pthread.h>
 
 namespace kivm {
     class AbstractThread {
+    private:
+        instanceOop _java_thread{};
+
     public:
         virtual ~AbstractThread() = default;
+
+        inline instanceOop get_java_thread() const {
+            return _java_thread;
+        }
+    };
+
+    class ThreadTable {
+    public:
     };
 
     class Thread : public AbstractThread {
