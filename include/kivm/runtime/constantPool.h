0a99853a8bf202ff13b6311c1327d93e757e8210
imkiva <libkernelpanic@gmail.com>
2018-03-31 23:59:02 +0800

    runtime: runtime constant table

--- include/kivm/runtime/constantPool.h
+++ include/kivm/runtime/constantPool.h
@@ -15,19 +15,32 @@ namespace kivm {
 
     class RuntimeConstantPool;
 
-    template<typename T, typename Creator>
-    class ConstantTable {
-        friend class RuntimeConstantPool;
+    namespace constant_table {
+        template<typename T, typename EntryType, typename Creator, int CONSTANT_TAG>
+        class ConstantTable {
+            friend class RuntimeConstantPool;
 
-    private:
-        cp_info **_raw_pool = nullptr;
+        private:
+            cp_info **_raw_pool = nullptr;
 
-        // constant-pool-index -> constant
-        std::unordered_map<int, T> _pool;
+            // constant-pool-index -> constant
+            std::unordered_map<int, T> _pool;
 
-    public:
-        virtual T findOrNew(int index) = 0;
-    };
+        public:
+            T findOrNew(int index) {
+                if (_raw_pool[index]->tag != CONSTANT_TAG) {
+                    PANIC("Accessing an incompatible constant entry");
+                }
+                const auto &iter = _pool.find(index);
+                if (iter != _pool.end()) {
+                    return iter->second;
+                }
+                T created = Creator((EntryType) _raw_pool[index]);
+                _pool.insert(std::make_pair(index, created));
+                return created;
+            }
+        };
+    }
 
     class RuntimeConstantPool {
     private:
