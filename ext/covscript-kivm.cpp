884d5ff9db4d7ae89480a86208fac0244135c931
imkiva <libkernelpanic@gmail.com>
2018-04-11 00:30:32 +0800

    ext: disable lint
    Signed-off-by: imkiva <libkernelpanic@gmail.com>

--- ext/covscript-kivm.cpp
+++ ext/covscript-kivm.cpp
@@ -4,19 +4,19 @@
 
 #include "extension_helper.h"
 
-CS_EXTENSION(kivm)
+CS_EXTENSION(kivm) // NOLINT
     struct kivm_holder {
     };
 
-    CNI_NORMAL(void, hello_world) {
+    CNI_NORMAL(void, hello_world) { // NOLINT
         printf("Hello World: CovScript\n");
     }
 
-    CNI_NORMAL(void, greet, cs::number x) {
+    CNI_NORMAL(void, greet, cs::number x) { // NOLINT
         printf("Hello World: %d\n", static_cast<int>(x));
     }
 
-    CNI_NORMAL(void, greet2, cs::number x, cs::number y) {
+    CNI_NORMAL(void, greet2, cs::number x, cs::number y) { // NOLINT
         printf("Hello World: x = %d, y = %d\n", static_cast<int>(x), static_cast<int>(y));
     }
 
