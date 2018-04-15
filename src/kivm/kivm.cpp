dd720fb16a90c67946df05585fe11dcc31dd9bcd
imkiva <libkernelpanic@gmail.com>
2018-04-16 00:27:27 +0800

    kivm: fill JavaVM structures
    Signed-off-by: imkiva <libkernelpanic@gmail.com>

--- src/kivm/kivm.cpp
+++ src/kivm/kivm.cpp
@@ -2,11 +2,21 @@
 // Created by kiva on 2018/4/16.
 //
 #include <kivm/kivm.h>
+#include <kivm/jni/jniJavaVM.h>
 
 namespace kivm {
     int KiVM::createVirtualMachine(JavaVM **pJavaVM) {
         // TODO: create VM
-        PANIC("createVirtualMachine() is not implemented");
+        auto invokeInterface = new JNIInvokeInterface_;
+        invokeInterface->AttachCurrentThread = JVM_ENTRY_NAME(AttachCurrentThread);
+        invokeInterface->AttachCurrentThreadAsDaemon = JVM_ENTRY_NAME(AttachCurrentThreadAsDaemon);
+        invokeInterface->DestroyJavaVM = JVM_ENTRY_NAME(DestroyJavaVM);
+        invokeInterface->DetachCurrentThread = JVM_ENTRY_NAME(DetachCurrentThread);
+        invokeInterface->GetEnv = JVM_ENTRY_NAME(GetEnv);
+
+        auto javaVM = new JavaVM;
+        javaVM->functions = invokeInterface;
+        *pJavaVM = javaVM;
         return JNI_OK;
     }
 }
