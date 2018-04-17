fbfa0f9a04a4c3ffb20d48984e08da6c9cc44623
imkiva <libkernelpanic@gmail.com>
2018-04-17 23:53:20 +0800

    bin: remove useless code
    Signed-off-by: imkiva <libkernelpanic@gmail.com>

--- src/bin/java.cpp
+++ src/bin/java.cpp
@@ -4,30 +4,6 @@
 
 int main() {
     using namespace kivm;
-    auto *integer = (InstanceKlass *) BootstrapClassLoader::get()
-            ->loadClass(L"java/lang/Integer");
-
-    auto *a = (InstanceKlass *) BootstrapClassLoader::get()
-            ->loadClass(L"java/lang/String");
-
-    auto *b = (InstanceKlass *) BootstrapClassLoader::get()
-            ->loadClass(L"[Ljava/lang/String;");
-
-    auto *c = (InstanceKlass *) BootstrapClassLoader::get()
-            ->loadClass(L"[[[[[Ljava/lang/String;");
-
-    auto *d = (InstanceKlass *) BootstrapClassLoader::get()
-            ->loadClass(L"[J");
-
-    auto *e = (InstanceKlass *) BootstrapClassLoader::get()
-            ->loadClass(L"[[[[[J");
-
-    auto *x = (InstanceKlass *) BootstrapClassLoader::get()
-        ->loadClass(L"java/lang/InterruptedException");
-
-    auto *y = (InstanceKlass *) BootstrapClassLoader::get()
-        ->loadClass(L"java/lang/System");
-
     JavaMainThread javaMainThread;
     javaMainThread.create(nullptr);
     return 0;
