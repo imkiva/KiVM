37d92364f10063a8c3c421614d7c9624576b99df
imkiva <libkernelpanic@gmail.com>
2018-04-18 00:39:06 +0800

    native: Change signatures to accept JNIEnv and jclass/jobject
    Signed-off-by: imkiva <libkernelpanic@gmail.com>

--- src/kivm/native/java_lang_Thread.cpp
+++ src/kivm/native/java_lang_Thread.cpp
@@ -4,8 +4,14 @@
 
 #include <kivm/native/java_lang_Thread.h>
 #include <kivm/kivm.h>
+#include <kivm/bytecode/execution.h>
 
-extern "C" jobject Java_java_lang_Thread_currentThread() {
+extern "C" jobject Java_java_lang_Thread_currentThread(JNIEnv *env, jclass java_lang_Thread) {
+    auto instanceKlass = kivm::Resolver::resolveJClass(java_lang_Thread);
+    D("JNIEnv: %p, I am %s",
+      env,
+      kivm::strings::toStdString(instanceKlass->getName()).c_str());
+
     PANIC("Native method: java/lang/Thread.currentThread() not implemented");
 }
 
