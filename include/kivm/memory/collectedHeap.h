69f0f81c8b4cfa1e9b4ec9f7d786ba66d23f666d
imkiva <libkernelpanic@gmail.com>
2018-04-19 23:44:12 +0800

    memory: create memory module
    Signed-off-by: imkiva <libkernelpanic@gmail.com>

---
+++ include/kivm/memory/collectedHeap.h
@@ -0,0 +1,11 @@
+//
+// Created by kiva on 2018/4/19.
+//
+#pragma once
+
+namespace kivm {
+    class CollectedHeap {
+    public:
+        virtual ~CollectedHeap() = default;
+    };
+}
---
+++ include/kivm/memory/markSweep.h
@@ -0,0 +1,12 @@
+//
+// Created by kiva on 2018/4/19.
+//
+#pragma once
+
+#include <kivm/memory/collectedHeap.h>
+
+namespace kivm {
+    class MarkSweepHeap : public CollectedHeap {
+
+    };
+}
