6f8019fd5a221eeab785b9dcf00a699adf3ac15f
imkiva <libkernelpanic@gmail.com>
2018-04-19 23:45:07 +0800

    memory: collectedHeap::allocate
    Signed-off-by: imkiva <libkernelpanic@gmail.com>

--- include/kivm/memory/collectedHeap.h
+++ include/kivm/memory/collectedHeap.h
@@ -3,9 +3,13 @@
 //
 #pragma once
 
+#include <kivm/kivm.h>
+
 namespace kivm {
     class CollectedHeap {
     public:
         virtual ~CollectedHeap() = default;
+
+        virtual void *allocate(size_t size) = 0;
     };
 }
