b954e7b2545f1594c6c70b648de001d642e8d56c
imkiva <libkernelpanic@gmail.com>
2018-04-27 23:56:23 +0800

    native: Object.hashCode()
    Signed-off-by: imkiva <libkernelpanic@gmail.com>

--- src/kivm/native/java_lang_Object.cpp
+++ src/kivm/native/java_lang_Object.cpp
@@ -10,3 +10,9 @@ using namespace kivm;
 JAVA_NATIVE void Java_java_lang_Object_registerNatives(JNIEnv *env, jclass java_lang_Object) {
     D("java/lang/Object.registerNatives()V");
 }
+
+JAVA_NATIVE jint Java_java_lang_Object_hashCode(JNIEnv *env, jobject javaObject) {
+    D("java/lang/Object.hashCode()I");
+    return 0;
+}
+
