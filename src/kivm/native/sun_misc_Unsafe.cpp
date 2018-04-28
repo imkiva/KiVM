5ae31d5a3ede3a8140c239912bee22aa7efd644c
imkiva <libkernelpanic@gmail.com>
2018-04-29 00:33:19 +0800

    native: sun.misc.Unsafe
    Signed-off-by: imkiva <libkernelpanic@gmail.com>

--- src/kivm/native/sun_misc_Unsafe.cpp
+++ src/kivm/native/sun_misc_Unsafe.cpp
@@ -11,8 +11,20 @@ JAVA_NATIVE void Java_sun_misc_Unsafe_registerNatives(JNIEnv *env, jclass sun_mi
     D("sun/misc/Unsafe.registerNatives()V");
 }
 
-JAVA_NATIVE jint Java_sun_misc_Unsafe_arrayBaseOffset(JNIEnv *env, jclass sun_misc_Unsafe) {
+JAVA_NATIVE jint Java_sun_misc_Unsafe_arrayBaseOffset(JNIEnv *env, jobject javaUnsafe,
+                                                      jobject mirror) {
     D("sun/misc/Unsafe.arrayBaseOffset(Ljava/lang/Class;)I");
     return 0;
+}
+
+JAVA_NATIVE jint Java_sun_misc_Unsafe_arrayIndexScale(JNIEnv *env, jobject javaUnsafe,
+                                                      jobject mirror) {
+    D("sun/misc/Unsafe.arrayIndexScale(Ljava/lang/Class;)I");
+    return sizeof(intptr_t);
+}
+
+JAVA_NATIVE jint Java_sun_misc_Unsafe_addressSize(JNIEnv *env, jobject javaUnsafe) {
+    D("sun/misc/Unsafe.addressSize()I");
+    return sizeof(intptr_t);
 }
 
