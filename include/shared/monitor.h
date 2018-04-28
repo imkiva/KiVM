b41d7bdd53ad1ad247f22ebc99ba86150c208a5b
imkiva <libkernelpanic@gmail.com>
2018-04-28 21:39:31 +0800

    shared: try to use recursive monitor
    Signed-off-by: imkiva <libkernelpanic@gmail.com>

--- include/shared/monitor.h
+++ include/shared/monitor.h
@@ -5,20 +5,30 @@
 
 #pragma once
 
+#include <shared/lock.h>
 #include <condition_variable>
 #include <thread>
 #include <mutex>
 #include <chrono>
 
 namespace kivm {
+    /**
+     * StackOverflow:
+     *
+     * In the case of recursive mutex,
+     * you do have to ensure that the given thread has only locked the recursive mutex once,
+     * since the condition variable only will use the unlock method
+     * on the unique_lock once during the wait.
+     */
+
     class Monitor final {
     private:
-        std::mutex _mutex;
-        std::condition_variable _cond;
-        std::unique_lock<std::mutex> _lock;
+        RecursiveLock _mutexLock;
+        std::unique_lock<RecursiveLock> _lock;
+        std::condition_variable_any _cond;
 
     public:
-        Monitor() : _lock(_mutex, std::defer_lock) {
+        Monitor() : _lock(_mutexLock, std::defer_lock) {
         }
 
         Monitor(const Monitor &) = delete;
