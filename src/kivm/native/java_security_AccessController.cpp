387993275cda9be6a10f40853a7f3fbb25297e42
imkiva <libkernelpanic@gmail.com>
2018-05-01 00:05:55 +0800

    native: AccessController.doPrivileged()
    Signed-off-by: imkiva <libkernelpanic@gmail.com>

--- src/kivm/native/java_security_AccessController.cpp
+++ src/kivm/native/java_security_AccessController.cpp
@@ -12,5 +12,28 @@ JAVA_NATIVE jobject Java_java_security_AccessController_doPrivileged(JNIEnv *env
                                                                      jclass java_security_AccessController,
                                                                      jobject javaPrivilegedExceptionAction) {
     auto actionOop = Resolver::resolveInstance(javaPrivilegedExceptionAction);
+    if (actionOop == nullptr) {
+        PANIC("java.lang.NullPointerException");
+        return nullptr;
+    }
 
+    // there are so many versions of doPrivileged().
+    // but we can do the simplest
+    auto actionClass = (InstanceKlass *) actionOop->getClass();
+    auto run = actionClass->getVirtualMethod(L"run", L"()Ljava/lang/Object;");
+    if (run == nullptr) {
+        PANIC("no run() method found");
+        return nullptr;
+    }
+
+    auto currentThread = (JavaThread *) Threads::currentThread();
+    if (currentThread == nullptr) {
+        PANIC("currentThread cannot be null");
+    }
+
+    D("native: AccessController.doPrivileged(): performing privileged actions");
+    oop result = currentThread->runMethod(run, {actionOop});
+
+    // TODO: check whether exception occurred
+    return result;
 }
