a36cad523f1302239a141aa5f2a9051882a66669
imkiva <libkernelpanic@gmail.com>
2018-05-13 16:48:49 +0800

    fix: cmpxchg not declared on aarch64

--- include/shared/atomic.h
+++ include/shared/atomic.h
@@ -7,6 +7,8 @@
 
 #if KIVM_ARCH_arm
 #include <shared/arch/arm/atomic.h>
+#elif KIVM_ARCH_aarch64
+#include <shared/arch/aarch64/atomic.h>
 #elif KIVM_ARCH_x86
 #include <shared/arch/x86/atomic.h>
 #elif KIVM_ARCH_x86_64
