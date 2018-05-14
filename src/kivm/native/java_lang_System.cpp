78e7d767ab8e341cc07313241762d204eb6458c9
imkiva <libkernelpanic@gmail.com>
2018-05-14 23:58:03 +0800

    native: initialize properties
    Signed-off-by: imkiva <libkernelpanic@gmail.com>

--- src/kivm/native/java_lang_System.cpp
+++ src/kivm/native/java_lang_System.cpp
@@ -15,36 +15,36 @@ using namespace kivm;
 JAVA_NATIVE jobject
 Java_java_lang_System_initProperties(JNIEnv *env, jclass java_lang_System, jobject propertiesObject) {
     static std::unordered_map<String, String> PROPS{
-        {L"java.vm.specification.name",  L"Java Virtual Machine Specification"},
-        {L"java.vm.specification.version",  L"1.8"},
-        {L"java.vm.version",  L"0.1.0"},
-        {L"java.vm.name",  L"Kiva's Java VM"},
-        {L"java.vm.info",  L"interpreted mode, sharing"},
-        {L"java.specification.version",  L"1.8"},
-        {L"java.version",  L"1.8.0-debug"},
-        {L"java.runtime.name",  L"Java(TM) SE Runtime Environment"},
-        {L"java.runtime.version",  L"1.8.0"},
-        {L"gopherProxySet",  L"false"},
-        {L"java.vm.vendor",  L"imKiva"},
-        {L"java.vendor.url",  L"https://github.com/imkiva"},
-        {L"path.separator",  Global::PATH_SEPARATOR},
-        {L"file.encoding.pkg",  L"sun.io"},
-        {L"user.country",  L"US"},
-        {L"user.language",  L"en"},
-        {L"sun.java.launcher",  L"KIVM_LAUNCHER"},
-        {L"sun.os.patch.level",  L"unknown"},
-        {L"os.arch",  KIVM_ARCH_NAME},
-        {L"sun.arch.data.model",  KIVM_ARCH_DATA_MODEL},
-        {L"line.separator",  L"\n"},
-        {L"file.separator",  Global::PATH_DELIMITER},
-        {L"sun.jnu.encoding",  L"UTF-8"},
-        {L"file.encoding",  L"UTF-8"},
-        {L"java.specification.name",  L"Java Platform API Specification"},
-        {L"java.class.version",  L"52.0"},
-        {L"sun.management.compiler",  L"nop"},
-        {L"sun.io.unicode.encoding",  L"UnicodeBig"},
-        {L"java.home",  L"."},
-        {L"java.class.path",  L"."},
+        {L"java.vm.specification.name",    L"Java Virtual Machine Specification"},
+        {L"java.vm.specification.version", L"1.8"},
+        {L"java.vm.version",               L"0.1.0"},
+        {L"java.vm.name",                  L"Kiva's Java VM"},
+        {L"java.vm.info",                  L"interpreted mode, sharing"},
+        {L"java.specification.version",    L"1.8"},
+        {L"java.version",                  L"1.8.0-debug"},
+        {L"java.runtime.name",             L"Java(TM) SE Runtime Environment"},
+        {L"java.runtime.version",          L"1.8.0"},
+        {L"gopherProxySet",                L"false"},
+        {L"java.vm.vendor",                L"imKiva"},
+        {L"java.vendor.url",               L"https://github.com/imkiva"},
+        {L"path.separator",                Global::PATH_SEPARATOR},
+        {L"file.encoding.pkg",             L"sun.io"},
+        {L"user.country",                  L"US"},
+        {L"user.language",                 L"en"},
+        {L"sun.java.launcher",             L"KIVM_LAUNCHER"},
+        {L"sun.os.patch.level",            L"unknown"},
+        {L"os.arch",             KIVM_ARCH_NAME},
+        {L"sun.arch.data.model", KIVM_ARCH_DATA_MODEL},
+        {L"line.separator",                L"\n"},
+        {L"file.separator",                Global::PATH_DELIMITER},
+        {L"sun.jnu.encoding",              L"UTF-8"},
+        {L"file.encoding",                 L"UTF-8"},
+        {L"java.specification.name",       L"Java Platform API Specification"},
+        {L"java.class.version",            L"52.0"},
+        {L"sun.management.compiler",       L"nop"},
+        {L"sun.io.unicode.encoding",       L"UnicodeBig"},
+        {L"java.home",                     L"."},
+        {L"java.class.path",               L"."},
     };
 
     auto propOop = Resolver::instance(propertiesObject);
@@ -52,7 +52,20 @@ Java_java_lang_System_initProperties(JNIEnv *env, jclass java_lang_System, jobje
         L"put", L"(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;");
 
     auto thread = (JavaThread *) Threads::currentThread();
-    InvocationContext::invokeWithArgs(thread, put, {});
+
+    if (thread == nullptr) {
+        PANIC("thread cannot be null");
+    }
+
+    for (const auto &e : PROPS) {
+        D("initProperties: set %s to %s",
+            strings::toStdString(e.first).c_str(),
+            strings::toStdString(e.second).c_str());
+        std::list<oop> args{propOop,
+                            java::lang::String::intern(e.first),
+                            java::lang::String::intern(e.second)};
+        InvocationContext::invokeWithArgs(thread, put, args);
+    }
     return propertiesObject;
 }
 
