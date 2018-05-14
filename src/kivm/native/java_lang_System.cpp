fa2e9cff7309c93c8788f60d4e57dadc0475c49e
imkiva <libkernelpanic@gmail.com>
2018-05-14 23:53:26 +0800

    native: initialize properties
    Signed-off-by: imkiva <libkernelpanic@gmail.com>

--- src/kivm/native/java_lang_System.cpp
+++ src/kivm/native/java_lang_System.cpp
@@ -2,15 +2,57 @@
 // Created by kiva on 2018/4/21.
 //
 
+#include <compileTimeConfig.h>
 #include <kivm/kivm.h>
 #include <kivm/native/classNames.h>
 #include <kivm/bytecode/execution.h>
 #include <kivm/oop/arrayKlass.h>
 #include <kivm/oop/arrayOop.h>
+#include <kivm/bytecode/invocationContext.h>
 
 using namespace kivm;
 
-JAVA_NATIVE jobject Java_java_lang_System_initProperties(JNIEnv *env, jobject propertiesObject) {
+JAVA_NATIVE jobject
+Java_java_lang_System_initProperties(JNIEnv *env, jclass java_lang_System, jobject propertiesObject) {
+    static std::unordered_map<String, String> PROPS{
+        {L"java.vm.specification.name",  L"Java Virtual Machine Specification"},
+        {L"java.vm.specification.version",  L"1.8"},
+        {L"java.vm.version",  L"0.1.0"},
+        {L"java.vm.name",  L"Kiva's Java VM"},
+        {L"java.vm.info",  L"interpreted mode, sharing"},
+        {L"java.specification.version",  L"1.8"},
+        {L"java.version",  L"1.8.0-debug"},
+        {L"java.runtime.name",  L"Java(TM) SE Runtime Environment"},
+        {L"java.runtime.version",  L"1.8.0"},
+        {L"gopherProxySet",  L"false"},
+        {L"java.vm.vendor",  L"imKiva"},
+        {L"java.vendor.url",  L"https://github.com/imkiva"},
+        {L"path.separator",  Global::PATH_SEPARATOR},
+        {L"file.encoding.pkg",  L"sun.io"},
+        {L"user.country",  L"CN"},
+        {L"user.language",  L"zh"},
+        {L"sun.java.launcher",  L"KIVM_LAUNCHER"},
+        {L"sun.os.patch.level",  L"unknown"},
+        {L"os.arch",  KIVM_ARCH_NAME},
+        {L"sun.arch.data.model",  L"64"},
+        {L"line.separator",  L"\n"},
+        {L"file.separator",  Global::PATH_DELIMITER},
+        {L"sun.jnu.encoding",  L"UTF-8"},
+        {L"file.encoding",  L"UTF-8"},
+        {L"java.specification.name",  L"Java Platform API Specification"},
+        {L"java.class.version",  L"52.0"},
+        {L"sun.management.compiler",  L"nop"},
+        {L"sun.io.unicode.encoding",  L"UnicodeBig"},
+        {L"java.home",  L"."},
+        {L"java.class.path",  L"."},
+    };
+
+    auto propOop = Resolver::instance(propertiesObject);
+    auto put = ((InstanceKlass *) propOop->getInstanceClass())->getVirtualMethod(
+        L"put", L"(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;");
+
+    auto thread = (JavaThread *) Threads::currentThread();
+    InvocationContext::invokeWithArgs(thread, put, {});
     return propertiesObject;
 }
 
