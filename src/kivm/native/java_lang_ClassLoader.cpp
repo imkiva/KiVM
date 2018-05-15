4c5032beb2c88db04779153b94df2cf20b3e2866
imkiva <libkernelpanic@gmail.com>
2018-05-16 00:02:13 +0800

    native: java/lang/ClassLoader
    Signed-off-by: imkiva <libkernelpanic@gmail.com>

---
+++ src/kivm/native/java_lang_ClassLoader.cpp
@@ -0,0 +1,13 @@
+//
+// Created by kiva on 2018/5/16.
+//
+
+#include <kivm/kivm.h>
+#include <kivm/bytecode/execution.h>
+#include <kivm/native/classNames.h>
+
+using namespace kivm;
+
+JAVA_NATIVE void Java_java_lang_ClassLoader_registerNatives(JNIEnv *env, jclass java_lang_ClassLoader) {
+    D("java/lang/ClassLoader.registerNatives()V");
+}
