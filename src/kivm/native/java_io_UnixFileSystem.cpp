e8b50b9f7ceed50b4e402056996bf5031bea2da6
imkiva <libkernelpanic@gmail.com>
2018-05-26 23:48:07 +0800

    native: UnixFileSystem
    Signed-off-by: imkiva <libkernelpanic@gmail.com>

---
+++ src/kivm/native/java_io_UnixFileSystem.cpp
@@ -0,0 +1,12 @@
+//
+// Created by kiva on 2018/5/26.
+//
+
+#include <kivm/kivm.h>
+#include <kivm/native/classNames.h>
+
+using namespace kivm;
+
+JAVA_NATIVE void Java_java_io_UnixFileSystem_initIDs(JNIEnv *env, jclass java_io_FileInputStream) {
+    D("java/io/UnixFileSystem.initIDs()V");
+}
