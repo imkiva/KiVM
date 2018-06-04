e9e68d0f776d2eaadbabe0b8187d60a4873c4f13
imkiva <libkernelpanic@gmail.com>
2018-06-04 18:08:55 +0800

    memory: start to support copy garbage collection
    Signed-off-by: imkiva <libkernelpanic@gmail.com>

--- src/kivm/memory/copying.cpp
+++ src/kivm/memory/copying.cpp
@@ -92,6 +92,18 @@ namespace kivm {
     }
 
     void CopyingHeap::doGarbageCollection() {
-        D("CopyingHeap::doGarbageCollection()");
+        // GC-Roots include:
+        // 0. InstanceKlass::_staticFieldValues
+        // 1. InstanceKlass::_javaMirror (no primitive types' mirrors)
+        // 2. InstanceKlass::_javaLoader
+        // 3. InstanceKlass::_runtimePool's Strings
+        // 4. instanceOopDesc::_instanceFieldValues
+        // 5. arrayOopDesc::_elements
+        // 6. JavaThread::_javaThreadObject
+        // 7. JavaThread::_exceptionOop
+        // 8. JavaThread::_frames[0 ~ the last frame]::_locals
+        // 9. JavaThread::_frames[0 ~ the last frame]::_stack
+
+        PANIC("TODO: CopyingHeap::doGarbageCollection()");
     }
 }
