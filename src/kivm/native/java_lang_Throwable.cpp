236927b4f810163b64e9f66f0ae8e93134d1aed0
imkiva <libkernelpanic@gmail.com>
2018-06-07 12:13:04 +0800

    native: native methods related to stack trace
    Signed-off-by: imkiva <libkernelpanic@gmail.com>

--- src/kivm/native/java_lang_Throwable.cpp
+++ src/kivm/native/java_lang_Throwable.cpp
@@ -8,7 +8,17 @@
 
 using namespace kivm;
 
+// TODO: Stack trace support
+
 JAVA_NATIVE jobject Java_java_lang_Throwable_fillInStackTrace(JNIEnv *env, jobject javaThrowable, jint depth) {
     auto instanceOop = Resolver::instance(javaThrowable);
     return instanceOop;
+}
+
+JAVA_NATIVE jint Java_java_lang_Throwable_getStackTraceDepth(JNIEnv *env, jobject javaThrowable) {
+    return 0;
+}
+
+JAVA_NATIVE jobject Java_java_lang_Throwable_getStackTraceElement(JNIEnv *env, jobject javaThrowable, jint index) {
+    return nullptr;
 }
