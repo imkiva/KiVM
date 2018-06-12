55b5237f5b3e23ce85a15fa506389786febaeab9
imkiva <libkernelpanic@gmail.com>
2018-06-12 13:27:09 +0800

    build: add Makefile for java-src
    Signed-off-by: imkiva <libkernelpanic@gmail.com>

--- java-src/com/imkiva/kivm/ChineseTest.java
+++ java-src/com/imkiva/kivm/ChineseTest.java
@@ -2,6 +2,6 @@ package com.imkiva.kivm;
 
 public class ChineseTest {
     public static void main(String[] args) {
-        System.out.println("你好世界！");
+        System.out.println("你好世界！我爱 KiVM");
     }
 }
