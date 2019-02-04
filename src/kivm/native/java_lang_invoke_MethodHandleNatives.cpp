8f0d6a1ea8dfe3ceb8b8655cd451e6654b018c60
imkiva <libkernelpanic@gmail.com>
2019-02-04 20:46:34 +0800

    MethodHandleNatives: getConstant()
    Signed-off-by: imkiva <libkernelpanic@gmail.com>

--- src/kivm/native/java_lang_invoke_MethodHandleNatives.cpp
+++ src/kivm/native/java_lang_invoke_MethodHandleNatives.cpp
@@ -5,7 +5,20 @@
 #include <kivm/kivm.h>
 #include <kivm/native/classNames.h>
 
-JAVA_NATIVE void Java_java_lang_invoke_MethodHandleNatives_registerNatives(JNIEnv *env, jclass java_lang_Object) {
+JAVA_NATIVE void Java_java_lang_invoke_MethodHandleNatives_registerNatives(JNIEnv *env, jclass mhn) {
     D("java/lang/MethodHandleNatives.registerNatives()V");
 }
+
+JAVA_NATIVE jint Java_java_lang_invoke_MethodHandleNatives_getConstant(JNIEnv *env, jclass mhn, jint i) {
+    // Copied from hotspot vm
+    assert(i == 4);
+    return 0;
+}
+
+JAVA_NATIVE jobject Java_java_lang_invoke_MethodHandleNatives_resolve(JNIEnv *env, jclass mhn,
+                                                                      jobject memberName, jclass caller) {
+    // TODO: implement
+    return nullptr;
+}
+
 
