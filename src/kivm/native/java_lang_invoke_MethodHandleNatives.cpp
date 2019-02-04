71aeb361c09aa854164369ad440a875254b83247
imkiva <libkernelpanic@gmail.com>
2019-02-04 11:47:05 +0800

    MethodHandleNatives: registerNatives()
    Signed-off-by: imkiva <libkernelpanic@gmail.com>

--- src/kivm/native/java_lang_invoke_MethodHandleNatives.cpp
+++ src/kivm/native/java_lang_invoke_MethodHandleNatives.cpp
@@ -2,3 +2,10 @@
 // Created by kiva on 2019-02-04.
 //
 
+#include <kivm/kivm.h>
+#include <kivm/native/classNames.h>
+
+JAVA_NATIVE void Java_java_lang_invoke_MethodHandleNatives_registerNatives(JNIEnv *env, jclass java_lang_Object) {
+    D("java/lang/MethodHandleNatives.registerNatives()V");
+}
+
