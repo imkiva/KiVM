a80895ab6eba1f1fcca79dec6df32c08fa2f611c
imkiva <libkernelpanic@gmail.com>
2020-02-11 16:25:25 +0800

    native: FileDescriptor: fix with jdk1.8.0_231

--- src/kivm/native/java_io_FileDescriptor.cpp
+++ src/kivm/native/java_io_FileDescriptor.cpp
@@ -10,3 +10,8 @@ using namespace kivm;
 JAVA_NATIVE void Java_java_io_FileDescriptor_initIDs(JNIEnv *env, jclass java_io_FileDescriptor) {
     D("java/io/FileDescriptor.initIDs()V");
 }
+
+JAVA_NATIVE jlong Java_java_io_FileDescriptor_set(JNIEnv *env, jclass java_io_FileDescriptor, jint fd) {
+    // TODO: convert fd to file handle for windows
+    return fd;
+}
