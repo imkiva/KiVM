cd112f4ea3bb0417aa55ae5d01fc60658edb549b
imkiva <libkernelpanic@gmail.com>
2020-05-12 18:52:56 +0800

    tests: update thread test

--- java-src/com/imkiva/kivm/ThreadTest.java
+++ java-src/com/imkiva/kivm/ThreadTest.java
@@ -1,17 +1,87 @@
 package com.imkiva.kivm;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ThreadTest {
-    static class MyThread extends Thread {
+    private int nextGood = 0;
+    private List<Integer> goods = new ArrayList<>();
+
+    class Producer extends Thread {
+        int id;
+
+        public Producer(int id) {
+            this.id = id;
+        }
+
+        @Override
         public void run() {
-            for (int i = 0; i < 10; ++i) {
-                System.out.println(getName() + ": " + i);
+            while (true) {
+                synchronized (Main.class) {
+                    while (goods.size() == 10) {
+                        try {
+                            Main.class.wait();
+                        } catch (Exception ignore) {
+                        }
+                    }
+
+                    int good = nextGood++;
+                    goods.add(good);
+                    System.out.println("Producer #" + id + ": produced " + good);
+                    Main.class.notifyAll();
+                }
+
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        MyThread t = new MyThread();
-        t.start();
-        t.join();
+    class Consumer extends Thread {
+        int id;
+
+        public Consumer(int id) {
+            this.id = id;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                synchronized (Main.class) {
+                    while (goods.size() == 0) {
+                        try {
+                            Main.class.wait();
+                        } catch (Exception ignore) {
+                        }
+                    }
+                    int good = goods.get(0);
+                    goods.remove(0);
+                    System.out.println("Consumer #" + id + " consumed " + good);
+                    Main.class.notifyAll();
+                }
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new ThreadTest().test();
+    }
+
+    private void test() {
+        for (int i = 0; i < 3; i++) {
+            new Producer(i).start();
+        }
+        for (int i = 0; i < 10; i++) {
+            new Consumer(i).start();
+        }
     }
 }
+
